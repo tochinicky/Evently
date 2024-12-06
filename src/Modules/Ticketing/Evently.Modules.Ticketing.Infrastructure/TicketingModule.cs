a63cfbf927b12c9046220de5d0bb2101aa28c3f8
TOCHUKWU ONYEAMAH <TochukwuOnyeamah@globusbank.com>
2024-12-07 00:09:34 +0100

    resolved all dependencies

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -0,0 +1,27 @@
+﻿using Evently.Common.Presentation.Endpoints;
+using Evently.Modules.Ticketing.Application.Carts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Evently.Modules.Ticketing.Infrastructure;
+
+public static class TicketingModule
+{
+    public static IServiceCollection AddTicketingModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddInfrastructure(configuration);
+
+        services.AddEndpoints(Presentation.AssemblyReference.Assembly);
+
+        return services;
+    }
+
+#pragma warning disable S1172 // Unused method parameters should be removed
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+#pragma warning restore S1172 // Unused method parameters should be removed
+    {
+        services.AddSingleton<CartService>();
+    }
+}
